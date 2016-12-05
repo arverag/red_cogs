36fa94ede333f7dd8b34068bc986120da5b13874
Zlkva <keithfgilbert@gmail.com>
2016-12-05 15:53:55 -0600

    Update time.py

--- time/time.py
+++ time/time.py
@@ -2,23 +2,23 @@ import discord
 from discord.ext import commands
 
 class Timecog:
-    
+
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def time(self):
-    """What time is it currently in-game?"""
+        """This does stuff!"""
 
-    #Your code will go here
-    url = "https://www.timeanddate.com/time/zones/pst" #build the web adress
-    async with aiohttp.get(url) as response:
-        soupObject = BeautifulSoup(await response.text(), "html.parser")
-    try:
-        servertime = soupObject.find(class_='ctm-tz').find('span').find(id_='hourmin0').get_text()
-        await self.bot.say('GMS server time is ' + servertime)
-    except:
-        await self.bot.say("Couldn't load server time.")
+        #Your code will go here
+        url = "https://www.timeanddate.com/time/zones/pst" #build the web adress
+        async with aiohttp.get(url) as response:
+            soupObject = BeautifulSoup(await response.text(), "html.parser")
+        try:
+            servertime = soupObject.find(class_='ctm-tz').find('span').find(id_='hourmin0').get_text()
+            await self.bot.say('GMS server time is ' + servertime)
+        except:
+            await self.bot.say("Couldn't load server time.")
 
 def setup(bot):
     bot.add_cog(Timecog(bot))
