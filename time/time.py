58bb53d9832f9d8c4d5b646a9943fd1492a9e1af
Zlkva <keithfgilbert@gmail.com>
2016-12-05 17:59:24 -0600

    Create time.py

---
+++ time/time.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+try: # check if BeautifulSoup4 is installed
+    from bs4 import BeautifulSoup
+    soupAvailable = True
+except:
+    soupAvailable = False
+import aiohttp
+
+class Timecog:
+    """My custom cog that does stuff!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def time(self):
+
+        #Your code will go here
+        url = "https://www.timeanddate.com/time/aboututc.html" #build the web adress
+        async with aiohttp.get(url) as response:
+            soupObject = BeautifulSoup(await response.text(), "html.parser")
+        try:
+            servertime = soupObject.find(class_='ctm-tz').find('span').get_text()
+            await self.bot.say('GMS server time is ' + servertime)
+        except:
+            await self.bot.say("Couldn't load server time.")
+
+def setup(bot):
+    bot.add_cog(Timecog(bot))
