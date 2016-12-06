b2b86da448fb8170c60b5d8cd9a37cebde53efd6
Zlkva <keithfgilbert@gmail.com>
2016-12-06 12:08:05 -0600

    Update status.py

--- status/status.py
+++ status/status.py
@@ -20,7 +20,7 @@ class Sstatus:
 
         if result == 0:
             elapsed_time = time.process_time() - t
-            await self.bot.say('Server responded in ' .format(elapsed_time) ' seconds.')
+            await self.bot.say('Server responded in ' + str(elapsed_time) + ' seconds.')
         s.close()
 
 
