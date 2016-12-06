5cbd3d9251475ecdf57b7df113d753aae05acb0e
Zlkva <keithfgilbert@gmail.com>
2016-12-05 22:31:34 -0600

    Update status.py

--- status/status.py
+++ status/status.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import socket
-from struct import pack, unpack
     
 class Sstatus:
     """Finds the current in-game server status"""
@@ -12,25 +11,17 @@ class Sstatus:
     @commands.command()
     async def serverstatus(self):
 
-        #Your code will go here
-        host = socket.gethostbyname("8.31.99.143")
-        port = 8484
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((host, port))
+        start = time.clock()
+        result = s.connect_ex(('127.0.0.1', 3306))
 
-        packet = ""
-        packet += pack('i',4)
-        packet += pack('p',port)
-        packet += pack('H',host)
-        packet += pack('i',1)
-        s.send(packet)
-        serverstatus = s.recv(1024) # Recv nothing ?
-
-        try:
-            await self.bot.say('Status = ' + serverstatus)
-        except:
-            await self.bot.say("Couldn't load server status.")
-
+        if result == 0:
+            await self.bot.say('Server responded in ', time.clock()-start ,' seconds')
+        s.close()
+        else result == 1:
+            await self.bot.say("Server no respondo")
+        s.close()               
+        
 def setup(bot):
     bot.add_cog(Sstatus(bot))
