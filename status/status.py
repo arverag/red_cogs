a8188a9bf2e09043aed79a8fe7ccf25395e737c9
Zlkva <keithfgilbert@gmail.com>
2016-12-06 12:21:55 -0600

    Update status.py

--- status/status.py
+++ status/status.py
@@ -20,7 +20,7 @@ class Sstatus:
 
         if result == 0:
             elapsed_time = time.clock() - t
-            await self.bot.say('Server responded in ' + format(elapsed_time, '.3f') + ' seconds.')
+            await self.bot.say('GMS Login Server responded in ' + format(elapsed_time, '.3f') + ' seconds.')
         s.close()
 
 
