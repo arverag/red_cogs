d6870db51dca88b955a930d69a6f140bde5c7a0d
Zlkva <keithfgilbert@gmail.com>
2016-12-09 14:41:15 -0600

    Update f.py

--- f/f.py
+++ f/f.py
@@ -1,1 +1,18 @@
-f
+import discord
+from discord.ext import commands
+
+class myFF:
+    """Meh"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ff(self):
+        """Butts"""
+
+        #Your code will go here
+        await self.bot.say("I can do stuff!")
+
+def setup(bot):
+    bot.add_cog(myFF(bot))
