cac1c15416587c66240ecdd9768cab5efcdba0e9
Zlkva <keithfgilbert@gmail.com>
2016-12-10 17:09:16 -0600

    Update f.py

--- f/f.py
+++ f/f.py
@@ -1,17 +1,1 @@
-import discord
-from discord.ext import commands
 
-class myFF:
-    """Meh"""
-
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command()
-    async def ff(self):
-
-        #Your code will go here
-        await self.bot.say(":saughting:")
-
-def setup(bot):
-    bot.add_cog(myFF(bot))
