9e815749eb9d57e29209d628155bb6f97b457c85
Zlkva <keithfgilbert@gmail.com>
2016-12-10 17:27:01 -0600

    Update status.py

--- status/status.py
+++ status/status.py
@@ -24,7 +24,29 @@ class Sstatus:
         else:
             await self.bot.say('The GMS Login Server did not respond in time :triumph: ')
         s.close()
+        
+class RBstatus:
+    """Finds the current in-game server status"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def rebootstatus(self):
+
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        t = time.clock()
+        result = s.connect_ex(('8.31.99.161', 8585))
+
+        if result == 0:
+            elapsed_timeRB = time.clock() - t
+            await self.bot.say('Reboot Channel 1 responded in ' + format(elapsed_timeRB, '.3f') + ' seconds :thumbsup:')
+        else:
+            await self.bot.say('Reboot Channel 1 did not respond in time :triumph: ')
+        s.close()
 
 
 def setup(bot):
     bot.add_cog(Sstatus(bot))
+    bot.add_cog(RBstatus(bot))
