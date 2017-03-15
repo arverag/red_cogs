8fe42fa72686149d5b4def9d4b91cbaaede0dfa6
Zlkva <keithfgilbert@gmail.com>
2017-03-15 12:34:12 -0500

    Create sotcraft.py

---
+++ sotcraft/sotcraft.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import aiohttp
+import json
+import urllib
+url = "https://mcapi.ca/query/47.186.163.211/extensive"
+
+class usersOnline:
+    """Finds users that are online in Sotcraft"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def sotcraft(self):
+
+        #Your code will go here
+        response = urllib.urlopen(url)    
+        data = json.loads(response.read())
+        onlineUsers - data["list"]
+        try:
+            await self.bot.say('Users Online: ' + onlineUsers)
+        except:
+            await self.bot.say("An error occurred")
+
+def setup(bot):
+    bot.add_cog(usersOnline(bot))
