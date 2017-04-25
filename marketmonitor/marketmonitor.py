7c19a6a5abd8010e2ea965d771d0ad97259015a5
Zlkva <keithfgilbert@gmail.com>
2017-04-25 11:39:14 -0500

    Update marketmonitor.py

--- marketmonitor/marketmonitor.py
+++ marketmonitor/marketmonitor.py
@@ -14,12 +14,14 @@ class MMonitor:
     def __init__(self, bot):
         self.bot = bot
         self.open_rifts = {}
+    
+    msgfilter = ['$', 'pp', 'paypal', 'moneypak', 'giftcard', 'gift card', 'PM me', 'DM', 'cash']
 
     @commands.command(pass_context=True)
     async def monitor(self, ctx, channel):
-        """Makes you able to communicate with other channels through Red
+        """Allows you to monitor channels through Red
 
-        This is cross-server. Type only the channel name or the ID."""
+        This is cross-server. Type only the channel name or the ID that you want the reports to exit from."""
         author = ctx.message.author
         author_channel = ctx.message.channel
 
@@ -62,11 +64,10 @@ class MMonitor:
         self.open_rifts[author] = rift
         await self.bot.say("Monitor started")
         msg = ""
-        msgfilter = ['$', 'pp', 'paypal', 'moneypak', 'giftcard', 'gift card', 'PM me', 'DM', 'cash']
         while msg == "" or msg is not None:
             msg = await self.bot.wait_for_message(author=author,
                                                   channel=author_channel)
-            if msg is not None and msg.content.lower() in msgfilter and msg.content.lower() != "exit":
+            if msg is not None and msg.content.lower() != "exit" and msg.content.lower() in msgfilter:
                 try:
                     await self.bot.say("Your message may contain words referring to RMT. Your message has been logged and will be reviewed by Discord staff.")
                 except:
@@ -79,11 +80,13 @@ class MMonitor:
     async def on_message(self, message):
         if message.author == self.bot.user:
             return
+        if message.content not in msgfilter:
+            return
         for k, v in self.open_rifts.items():
-            if v.destination == message.channel:
+            if v.source == message.channel:
                 msg = "**__{}__**: {}".format(message.author, message.content)
                 msg = escape(msg, mass_mentions=True)
-                await self.bot.send_message(v.source, msg)
+                await self.bot.send_message(v.destination, msg)
 
 def setup(bot):
     bot.add_cog(MMonitor(bot))
