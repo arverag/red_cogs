67600dad4f39ca178f08b32bbd6d000aeea4a865
Zlkva <keithfgilbert@gmail.com>
2017-04-25 11:45:56 -0500

    Update marketmonitor.py

--- marketmonitor/marketmonitor.py
+++ marketmonitor/marketmonitor.py
@@ -66,7 +66,7 @@ class MMonitor:
         while msg == "" or msg is not None:
             msg = await self.bot.wait_for_message(author=author,
                                                   channel=author_channel)
-            if msg.content.lower() in msgfilter:
+            if msg.content.lower() is in msgfilter:
                 try:
                     await self.bot.say("Your message may contain words referring to RMT. Your message has been logged and will be reviewed by Discord staff.")
                 except:
@@ -82,10 +82,10 @@ class MMonitor:
         if message.content not in msgfilter:
             return
         for k, v in self.open_rifts.items():
-            if v.source == message.channel:
+            if v.destination == message.channel:
                 msg = "**__{}__**: {}".format(message.author, message.content)
                 msg = escape(msg, mass_mentions=True)
-                await self.bot.send_message(v.source, msg)
+                await self.bot.send_message(v.destination, msg)
 
 def setup(bot):
     bot.add_cog(MMonitor(bot))
