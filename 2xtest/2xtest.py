ee4ec8a97fb96f917169dd1fc35a3a26366fddae
Zlkva <keithfgilbert@gmail.com>
2017-06-15 20:14:10 -0500

    Update 2xtest.py

--- 2xtest/2xtest.py
+++ 2xtest/2xtest.py
@@ -17,5 +17,53 @@ class twoXcog:
     @commands.command()
     async def test2x(self):
 
+        linkPart = []
+        mainPage = ("https://e8tdwagy36.execute-api.us-west-2.amazonaws.com/prod/getTimeUntilNextEvent")
+        try:
+            try:
+                htmltext = urllib.request.urlopen(mainPage).read()
+                regex = '<pre>(.*?)</pre>'
+                matches = re.findall(re.compile(regex), htmltext)
+                match = "none"
+
+                endTimes = []
+
+                for i in range(0, len(matches)):
+                    endTimes.append(matches)
+
+                if match == "none":
+                    match = []
+                    raise Exception()
+            except:
+                await self.bot.say("The next 2x event has not been announced yet in a supported format.")
+
+            eventPage = "https://e8tdwagy36.execute-api.us-west-2.amazonaws.com/prod/getTimeUntilNextEvent"
+            try:
+                htmltext = urllib.request.urlopen(eventPage).read()
+                regex = '<pre>(.*?)</pre>'
+                htmltext = re.findall(re.compile(regex), htmltext)
+
+                regex = '<pre>(.*?)</pre>'
+                matches = re.findall(re.compile(regex), htmltext)
+
+                stringtosay = ".."
+
+                if len(endTimes) == 0:
+                    stringtosay = stringtosay + "\r\nThe next 2x event has not been announced yet."
+
+                except:
+                    nextEndtime = endTimes[0]
+                    if len(startTimes) == 0 or startTimes[0] - nextEndtime > timedelta(seconds=0):
+                        timeSpan = nextEndtime.replace(microsecond=0) - currenttimepst.replace(microsecond=0)
+                        stringtosay = stringtosay + " (" + endTimes + ")"
+
+                await self.bot.say(stringtosay)
+
+            except Exception as e:
+                await self.bot.say("Something broke! Try contacting @boardwalk hotel to get it fixed")
+                traceback.print_exc()
+        except:
+            e = 5
+
 def setup(bot):
     bot.add_cog(twoXcog(bot))
