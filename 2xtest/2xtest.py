0df0be60d342e6d5f8104d0d949cd6a0399247a9
Zlkva <keithfgilbert@gmail.com>
2017-06-15 19:55:15 -0500

    Update 2xtest.py

--- 2xtest/2xtest.py
+++ 2xtest/2xtest.py
@@ -7,16 +7,8 @@ import datetime
 import traceback
 from datetime import datetime, timedelta
 
-# assumptions (if any of these are false behavior is undefined)
-
-# news page will always have a 2x article
-# event page url contains the dates of the event (ex. 12-10-12-11) which are listed in pairs
-# daylight savings time does not exist (see below)
-# format of times is always ##:## AM or ##:## PM
-# times are always listed in pairs where the first is a start time and the second is an end time
-# 2x events take place in the current year (this code won't work if it's dec 31 and there's an event on jan 1)
-
 class twoXcog:
+
     """Finds the next 2x time"""
 
     def __init__(self, bot):
@@ -25,16 +17,13 @@ class twoXcog:
     @commands.command()
     async def test2x(self):
 
-        # will need to be changed to -7 during daylight savings
-        # or look into "pytz" module for more accurate pst/pdt calculation
-
         linkPart = []
         mainPage = ("https://e8tdwagy36.execute-api.us-west-2.amazonaws.com/prod/getTimeUntilNextEvent")
         try:
             try:
                 htmltext = urllib.request.urlopen(mainPage).read()
                 regex = '<pre>(.*?)</pre>'
-                matches = re.findall(re.compile(regex),htmltext)
+                matches = re.findall(re.compile(regex), htmltext)
                 match = "none"
 
                 endTimes = []
@@ -46,18 +35,17 @@ class twoXcog:
                     match = []
                     raise Exception()
             except:
-                await self.bot.say ("The next 2x event has not been announced yet in a supported format.")
+                await self.bot.say("The next 2x event has not been announced yet in a supported format.")
 
             eventPage = "https://e8tdwagy36.execute-api.us-west-2.amazonaws.com/prod/getTimeUntilNextEvent"
             try:
                 htmltext = urllib.request.urlopen(eventPage).read()
-                regex = '<pre>(.*?)</pre>' #separate 2x section
-                htmltext = re.findall(re.compile(regex),htmltext)
+                regex = '<pre>(.*?)</pre>'
+                htmltext = re.findall(re.compile(regex), htmltext)
 
-                regex = '<pre>(.*?)</pre>' #read months and days
-                matches = re.findall(re.compile(regex),htmltext)
+                regex = '<pre>(.*?)</pre>'
+                matches = re.findall(re.compile(regex), htmltext)
 
-                #print(currenttimepst, startTimes)
                 stringtosay = ".."
 
                 if len(endTimes) == 0:
@@ -72,10 +60,10 @@ class twoXcog:
                 await self.bot.say(stringtosay)
 
             except Exception as e:
-                await self.bot.say ("Something broke! Try contacting @boardwalk hotel to get it fixed")
+                await self.bot.say("Something broke! Try contacting @boardwalk hotel to get it fixed")
                 traceback.print_exc()
         except:
-            e = 5 #Try requires an except, and except requires one line.
+            e = 5
 
 def setup(bot):
     bot.add_cog(twoXcog(bot))
