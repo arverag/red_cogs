5c1ec2ece8d0ee1a9189aacd6372e8fcf699a3ad
Zlkva <keithfgilbert@gmail.com>
2017-07-03 10:09:56 -0500

    Update 2xtest.py

--- 2xtest/2xtest.py
+++ 2xtest/2xtest.py
@@ -24,14 +24,25 @@ class twoX:
         soup = BeautifulSoup(r.text, 'lxml')
         for datapull in soup.find_all('body'):
             if "nope" in datapull.text:
-                await self.bot.say("I don't think 2x is scheduled.")
+                embed = discord.Embed(title="**I don't think 2x is scheduled.**", color=self.red)
+                await self.bot.say(embed=embed)
             elif "!" in datapull.text:
                 newval = re.sub(r'!', "", datapull.text)
                 await self.bot.say("2x is active for another __**" + newval + "!**__")
-            else:
-                pulledtext = datapull.text
-                embed = discord.Embed(title='**The next 2x is scheduled in:**', description=pulledtext, color=self.green)
+                pulledtext = ("__**" + newval + "**__ left!")
+                embed = discord.Embed(title=':tada: **2x is active!** :tada:', description=pulledtext, color=self.green)
                 await self.bot.say(embed=embed)
+            else:
+                website2 = 'https://ijgfbhygk9.execute-api.us-west-2.amazonaws.com/prod/getEventTimes'
+                r2 = requests.get(website2)
+                soup2 = BeautifulSoup(r2.text, 'lxml')
+                for datapull2 in soup2.find_all('body'):
+                pulledtext2 = datapull2.text
+                embed2 = discord.Embed(title='**2x Schedule**', description=pulledtext2, color=self.orange)
+                pulledtext = datapull.text
+                embed = discord.Embed(title='**The next 2x is scheduled in:**', description=pulledtext, color=self.orange)
+                await self.bot.say(embed=embed)
+                await self.bot.say(embed=embed2)
     
     @commands.command()
     async def check2x(self):
