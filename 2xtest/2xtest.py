e42de336a41facba05ddea6f3223adb4371b9a7e
Zlkva <keithfgilbert@gmail.com>
2017-07-03 19:36:59 -0500

    Update 2xtest.py

--- 2xtest/2xtest.py
+++ 2xtest/2xtest.py
@@ -32,16 +32,40 @@ class twoX:
                 embed = discord.Embed(title=':tada: **2x is active!** :tada:', description=pulledtext, color=self.green)
                 await self.bot.say(embed=embed)
             else:
-                website2 = 'https://ijgfbhygk9.execute-api.us-west-2.amazonaws.com/prod/getEventTimes'
-                r2 = requests.get(website2)
-                soup2 = BeautifulSoup(r2.text, 'lxml')
-                for datapull2 in soup2.find_all('body'):
-                    pulledtext2 = datapull2.text
-                embed2 = discord.Embed(title='**2x Schedule**', description=pulledtext2, color=self.orange)
                 pulledtext = datapull.text
                 embed = discord.Embed(title='**The next 2x is scheduled in:**', description=pulledtext, color=self.orange)
                 await self.bot.say(embed=embed)
-                await self.bot.say(embed=embed2)
+                
+                website = 'https://ijgfbhygk9.execute-api.us-west-2.amazonaws.com/prod/getEventTimes'
+                r = requests.get(website)
+                soup = BeautifulSoup(r.text, 'lxml')
+                for datapull in soup.find_all('body'):
+                    pulledtext = datapull.text
+                    pulledsplit = pulledtext.split()
+                    pulledlen = len(pulledsplit)
+                    if pulledlen < 9:
+                        timesheet = "Nope"
+                        embed = discord.Embed(title='**2x Schedule**', description=timesheet, color=self.orange)
+                        await self.bot.say(embed=embed)
+                    elif pulledlen > 19 :
+                        timesheet = "The dumbass mod put too many 2x times on the schedule."
+                        embed = discord.Embed(title='**2x Schedule**', description=timesheet, color=self.orange)
+                        await self.bot.say(embed=embed)
+                    elif pulledlen >= 18 :
+                        timesheet = (pulledtext.split()[0] + " | " + pulledtext.split()[1] +" "+ pulledtext.split()[2] +"\n")
+                        timesheet += (pulledtext.split()[3] + " | " + pulledtext.split()[4] +" "+ pulledtext.split()[5] +"\n")
+                        timesheet += (pulledtext.split()[6] + " | " + pulledtext.split()[7] +" "+ pulledtext.split()[8] +"\n\n")
+                        timesheet += (pulledtext.split()[9] + " | " + pulledtext.split()[10] +" "+ pulledtext.split()[11] +"\n")
+                        timesheet += (pulledtext.split()[12] + " | " + pulledtext.split()[13] +" "+ pulledtext.split()[14] +"\n")
+                        timesheet += (pulledtext.split()[15] + " | " + pulledtext.split()[16] +" "+ pulledtext.split()[17] )
+                        embed = discord.Embed(title='**2x Schedule**', description=timesheet, color=self.orange)
+                        await self.bot.say(embed=embed)
+                    elif pulledlen >= 9 :
+                        timesheet = (pulledtext.split()[0] + " | " + pulledtext.split()[1] +" "+ pulledtext.split()[2] +"\n")
+                        timesheet += (pulledtext.split()[3] + " | " + pulledtext.split()[4] +" "+ pulledtext.split()[5] +"\n")
+                        timesheet += (pulledtext.split()[6] + " | " + pulledtext.split()[7] +" "+ pulledtext.split()[8] )
+                        embed = discord.Embed(title='**2x Schedule**', description=timesheet, color=self.orange)
+                        await self.bot.say(embed=embed)
     
     @commands.command()
     async def check2x(self):
